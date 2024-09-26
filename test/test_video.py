46a0db13766ddafe80aa05cf493fc74c98be9b8d
Lakshman <lakshman@gmail.com>
2024-09-26 15:52:05 +0000

    Refactor test_video_sam to improve detection assertions and remove unused ontology entry

--- test/test_video.py
+++ test/test_video.py
@@ -14,7 +14,6 @@ def grounded_sam_client():
     ontology = CaptionOntology(
         {
             "person": "person",
-            "shipping container": "shipping container",
         }
     )
     grounding_dino_box_threshold = 0.35
@@ -31,4 +30,15 @@ def grounded_sam_client():
 
 
 def test_video_sam(video_path, grounded_sam_client):
-    detections, lables = grounded_sam_client.predict(video_path)
+    detection = grounded_sam_client.predict(video_path)
+
+    assert detection.mask is not None
+    assert detection.class_id is not None
+    assert detection.confidence is not None
+    assert detection.tracker_id is not None
+    assert detection.xyxy is not None
+    assert detection.xyxy.shape[0] > 0
+    assert detection.mask.shape[0] > 0
+    assert detection.class_id.shape[0] > 0
+    assert detection.confidence.shape[0] > 0
+    assert detection.tracker_id.shape[0] > 0
